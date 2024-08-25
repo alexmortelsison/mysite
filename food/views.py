e68104bfed2bb44d3c0db8e56b168bc6156f8848
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-25 19:27:23 +0900

    updated

--- food/views.py
+++ food/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
 
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
